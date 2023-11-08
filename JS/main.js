113e609661116da919be751c541c695ad077d51f
RuxandraRambet <ruxandraramb@gmail.com>
2023-11-08 18:54:37 +0100

    feat: stampa in console

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const listElement = document.getElementById('square-list');
+
+for (let i = 1; i <= 100; i++) {
+    let squareElement = document.createElement('li');
+
+    if (i % 3 === 0 && 5 === 0) {
+        console.log('fizzbuzz');
+    }else if (i % 3 === 0) {
+        console.log('fizz');
+    }else if (i % 5 === 0) {
+        console.log('buzz');
+    }else {
+        console.log(i);
+    }
+}
