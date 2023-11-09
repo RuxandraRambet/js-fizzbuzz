dad55b60b8c7a1da07935d259118f4545476bb4f
RuxandraRambet <ruxandraramb@gmail.com>
2023-11-09 10:51:28 +0100

    feat: meno ripetizioni in js

--- JS/main.js
+++ JS/main.js
@@ -8,22 +8,25 @@ for (let i = 1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
         console.log('fizzbuzz');
         squareElement.append('fizzbuzz');
-        listElement.append(squareElement);
+        // Oppure
+        // squareElement.innerHTML = 'fizzbuzz';
+        // Oppure
+        // squareElement.innerText = 'fizzbuzz';
+
         squareElement.classList.add('square-fizzbuzz');
     }else if (i % 3 === 0) {
         console.log('fizz');
         squareElement.append('fizz');
-        listElement.append(squareElement);
         squareElement.classList.add('square-fizz');
     }else if (i % 5 === 0) {
         console.log('buzz');
         squareElement.append('buzz');
-        listElement.append(squareElement);
         squareElement.classList.add('square-buzz');
     }else {
         console.log(i);
         squareElement.append(i);
-        listElement.append(squareElement);
         squareElement.classList.add('square-number');
     }
+
+    listElement.append(squareElement);
 }
